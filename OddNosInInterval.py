627eaf95960299a9a842e5be943da9493e6de6aa
Sumashree M <sumashreemarx@gmail.com>
2017-06-21 17:09:16 +0530

    Create OddNosInInterval.py

---
+++ OddNosInInterval.py
@@ -0,0 +1,6 @@
+n1=int(input("Enter upper interval:"))
+n2=int(input("Enter lower interval:"))
+print("Odd numbers between "+str(n1)+" and " +str(n2))
+for i in range(n1,n2):
+   if i%2<>0:
+       print(i)
