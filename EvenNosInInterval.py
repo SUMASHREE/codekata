9663bd3275be2ee3e4e44f7d481cbc57b69d6d4f
Sumashree M <sumashreemarx@gmail.com>
2017-06-21 17:15:26 +0530

    Update EvenNosInInterval.py

--- EvenNosInInterval.py
+++ EvenNosInInterval.py
@@ -2,5 +2,5 @@ n1=int(input("Enter upper interval:"))
 n2=int(input("Enter lower interval:"))
 print("Even numbers between "+str(n1)+" and " +str(n2))
 for i in range(n1+1,n2):
-    if i%2<>0:
+    if i%2==0:
         print(i)
