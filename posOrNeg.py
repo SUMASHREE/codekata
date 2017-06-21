5f15c13d077cf9103980d212fa57e644da17347e
Sumashree M <sumashreemarx@gmail.com>
2017-06-21 11:35:55 +0530

    Update posOrNeg.py

--- posOrNeg.py
+++ posOrNeg.py
@@ -1,7 +1,7 @@
-    n=input("Enter a no:")
-    if n>0:
-        print("Positive number")
-    elif n==0:
-        print("Zero")
-    else:
-        print("Negative number")
+n=input("Enter a no:")
+if n>0:
+    print("Positive number")
+elif n==0:
+    print("Zero")
+else:
+    print("Negative number")
