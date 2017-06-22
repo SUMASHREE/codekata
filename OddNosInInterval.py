3cab077e204776871aaeb06d39178dbd7f0651d5
Sumashree M <sumashreemarx@gmail.com>
2017-06-22 09:25:04 +0530

    Update OddNosInInterval.py

--- OddNosInInterval.py
+++ OddNosInInterval.py
@@ -1,6 +1,6 @@
-n1=int(input("Enter upper interval:"))
-n2=int(input("Enter lower interval:"))
+n1=int(input("Enter lower interval:"))
+n2=int(input("Enter upper interval:"))
 print("Odd numbers between "+str(n1)+" and " +str(n2))
-for i in range(n1+!,n2):
-   if i%2<>0:
-       print(i)
+for i in range(n1+1,n2):
+    if i%2<>0:
+        print(i)
