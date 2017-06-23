e0677b5a9ff9878a812316cccc0b3e57d89ff089
Sumashree M <sumashreemarx@gmail.com>
2017-06-23 11:28:00 +0530

    Create calculateSI.py

---
+++ calculateSI.py
@@ -0,0 +1,5 @@
+   p=float(input("Enter the Principal:"))
+   n=float(input("Enter no of years:"))
+   r=float(input("Enter rate of interest:"))
+   amt=p*n*r/100
+   print("Simple Interest:"+str(amt))
