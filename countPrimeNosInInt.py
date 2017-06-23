882ceeb1b27534d88a1b4d21df9fc04559f26f1a
Sumashree M <sumashreemarx@gmail.com>
2017-06-23 10:05:36 +0530

    Create countPrimeNosInInt.py

---
+++ countPrimeNosInInt.py
@@ -0,0 +1,12 @@
+        count=0
+        n1=int(input("Enter lower interval:"))
+        n2=int(input("Enter upper interval:"))
+        print("Prime numbers:")
+        for i in range(n1+1,n2):
+            for j in range(2,i):
+                if i%j==0:
+                    break
+            else:
+                print(i)
+                count=count+1
+        print("Count of prime numbers between given range:"+str(count))
